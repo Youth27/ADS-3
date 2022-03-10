3ea9f22270338c5bc15748329dcc42c6dfffb4e5
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-03-10 11:00:28 +0300

    Артём Маричев

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,17 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+  int schetchik = 0, j = 0;
+  for (j; size > j; j++) {
+    if (arr[j] == value) {
+      schetchik++;
+    }
+  }
+  if (schetchik != 0) {
+    return schetchik;
+  } 
+  else {
+    return 0;
+  }
   return 0; // если ничего не найдено
 }
